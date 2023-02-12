0888f50cd2fa59637cdc048b9bc86d6b7208b787
Michael Pontus <m.pontus@gmail.com>
2023-02-12 18:22:21 +0300

    Create sample setup with node-dependency-injection

---
+++ src/Mailer.js
@@ -0,0 +1,5 @@
+export class Mailer {
+  constructor(transport) {
+    this.transport = transport;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+import { ContainerBuilder } from "node-dependency-injection";
+import { Mailer } from "./Mailer";
 
-export default function howLongUntilLunch(hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
+const container = new ContainerBuilder();
+container.register("mailer", Mailer, ["sendmail"]);
 
-	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
-}
+export { container };
